9a08d5b03d37587f8569d199ad53f1a1d1c367be
doctor <fd5@MacBook-Air-2022.local>
2023-12-22 16:05:31 +0700

    ok

--- src/components/MessageBoard.jsx
+++ src/components/MessageBoard.jsx
@@ -1,23 +1,49 @@
+import React, { useState } from "react";
+
 function MessageBoard() {
+  const [messages, setMessages] = useState([]);
+
+  const handleMessageSubmit = (e) => {
+    e.preventDefault();
+    const messageText = e.target.elements.messageText.value;
+    if (messageText) {
+      setMessages([...messages, messageText]);
+      e.target.reset();
+    }
+  };
+
+  const handleDeleteMessage = (index) => {
+    setMessages(messages.filter((_, i) => i !== index));
+  };
+
   return (
     <div className="app-wrapper">
-      <h1 class="app-title">Message board</h1>
-      <div class="message-input-container">
+      <h1 className="app-title">Message board</h1>
+      <form className="message-input-container" onSubmit={handleMessageSubmit}>
         <label>
           <input
             id="message-text"
-            name="message-text"
+            name="messageText"
             type="text"
             placeholder="Enter message here"
           />
         </label>
-        <button className="submit-message-button">Submit</button>
-      </div>
-      <div class="board">
-        <div className="message">
-          <h1>Hello all ! This is first message.</h1>
-          <button className="delete-button">x</button>
-        </div>
+        <button type="submit" className="submit-message-button">
+          Submit
+        </button>
+      </form>
+      <div className="board">
+        {messages.map((message, index) => (
+          <div className="message" key={index}>
+            <h1>{message}</h1>
+            <button
+              className="delete-button"
+              onClick={() => handleDeleteMessage(index)}
+            >
+              x
+            </button>
+          </div>
+        ))}
       </div>
     </div>
   );
